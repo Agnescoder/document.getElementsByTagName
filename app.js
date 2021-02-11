9cb3f926fe4ce2507458890951bb2457e69b58bc
Agnescoder <50987260+Agnescoder@users.noreply.github.com>
2021-02-11 18:20:27 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+const btn = document.querySelector("button");
+const liItems = document.querySelectorAll("li");
+
+// const liItems = [...document.getElementsByTagName("li")];
+// zamienione na tablice aby działało operator 'rest'
+
+let fontSize = 10;
+
+// Pętla
+// btn.addEventListener("click", () => {
+//     console.log("działa");
+
+//     fontSize++
+//     for (let i = 0; i < liItems.length; i++){
+//     //     if(!liItems[i].style.display){
+//     //         console.log("dodałam block");
+//     //     liItems[i].style.display = "block";
+//     // }
+
+//     liItems[i].style.display = "block"
+//     liItems[i].style.fontSize = `${fontSize}px`;
+//     }
+
+// })
+
+// ForEach
+btn.addEventListener("click", () => {
+    liItems.forEach((liItems)=> {
+        liItems.style.display = "block";
+        liItems.style.fontSize = fontSize + "px";
+    })
+    fontSize++;
+})
